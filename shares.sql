e466e3a289b3a614620f56e95a935a0371fb09e3
Dave Cross <dave@dave.org.uk>
2015-09-23 16:54:27 +0100

    Renamed share to instrument. Added price and instrument_type tables.

--- shares.sql
+++ shares.sql
@@ -1,15 +1,30 @@
-create table share (
+create table instrument_type (
   id integer primary key auto_increment,
+  code char(10) not null,
+  name varchar(255)
+) ENGINE=InnoDB;
+
+create table instrument (
+  id integer primary key auto_increment,
+  instrument_type_id integer not null,
   ticker char(10) not null,
-  name varchar(255) not null
+  name varchar(255) not null,
+  foreign key (instrument_type_id) references instrument_type(id)
+) ENGINE=InnoDB;
+
+create table price (
+  id integer primary key auto_increment,
+  instrument_id integer not null,
+  date datetime not null,
+  foreign key (instrument_id) references instrument(id)
 ) ENGINE=InnoDB;
 
 create table trade (
   id integer primary key auto_increment,
-  share_id integer not null,
+  instrument_id integer not null,
   date datetime not null,
   number integer not null,
   price decimal(20, 5) not null,
   fee decimal (5,2) not null,
-  foreign key (share_id) references share(id)
+  foreign key (instrument_id) references instrument(id)
 ) ENGINE=InnoDB;
